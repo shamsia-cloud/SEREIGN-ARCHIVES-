22d62335870b02042165ddab4f7981fe57b58d8b
shamsia-cloud <83899484+shamsia-cloud@users.noreply.github.com>
2026-09-08 23:07:27 +0100

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,62 @@
+fetch("../data/books.json")
+
+.then(response => response.json())
+
+.then(books => {
+
+
+const container =
+document.getElementById("book-container");
+
+
+books.forEach(book=>{
+
+
+let card=document.createElement("article");
+
+card.className="book-card";
+
+
+
+card.innerHTML=`
+
+<img src="../${book.cover}">
+
+
+<h2>
+${book.title}
+</h2>
+
+
+<p>
+${book.author}
+</p>
+
+
+<p class="price">
+
+${book.free ? "FREE" : "$"+book.price}
+
+</p>
+
+
+<a 
+class="book-button"
+href="reader.html?id=${book.id}">
+
+${book.free ? "Read Now":"Purchase"}
+
+</a>
+
+
+`;
+
+
+
+container.appendChild(card);
+
+
+});
+
+
+});
